9ce28d15e96350aad2e1be7ac2318bf1dc225214
Martina Ambrosini <martinaambrosini17@gmail.com>
2023-12-20 18:43:54 +0100

    add: AppTitle component

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "NewComponent"
+}
+</script>
+
+<template>
+    <h1 class="read-the-docs">Hello Vite in Vue, i'm new!!</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
